78f2a3ad562561e94f535d7161f00fc5b323dca9
Bernhard <bernhard@bermeb.dev>
2025-07-17 20:06:18 +0200

    fix icons to display correctly

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   base: '/',
   plugins: [
     react(),
@@ -13,6 +13,17 @@ export default defineConfig(({ mode }) => ({
         'favicon.ico',
         'favicon-16x16.png',
         'favicon-32x32.png',
+        'icon-48x48.png',
+        'icon-72x72.png',
+        'icon-96x96.png',
+        'icon-128x128.png',
+        'icon-144x144.png',
+        'icon-152x152.png',
+        'icon-192x192.png',
+        'icon-256x256.png',
+        'icon-384x384.png',
+        'icon-512x512.png',
+        'icon-1024x1024.png',
         'apple-touch-icon-180x180.png',
         'apple-touch-icon-167x167.png',
         'apple-touch-icon-152x152.png',
@@ -32,16 +43,71 @@ export default defineConfig(({ mode }) => ({
         start_url: '/',
         icons: [
           {
+            src: 'favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png'
+          },
+          {
+            src: 'favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png'
+          },
+          {
             src: 'icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
+            src: 'icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
             src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
+          },
+          {
+            src: 'icon-1024x1024.png',
+            sizes: '1024x1024',
+            type: 'image/png'
           },
           {
             src: 'maskable-icon-192x192.png',
@@ -54,6 +120,21 @@ export default defineConfig(({ mode }) => ({
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
+          },
+          {
+            src: 'apple-touch-icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png'
+          },
+          {
+            src: 'apple-touch-icon-167x167.png',
+            sizes: '167x167',
+            type: 'image/png'
+          },
+          {
+            src: 'apple-touch-icon-180x180.png',
+            sizes: '180x180',
+            type: 'image/png'
           }
         ]
       },
